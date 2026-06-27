3e9ad90a3b0359b02476013499f355917baf1359
Ubuntu <ubuntu@ip-172-31-17-2.ec2.internal>
2026-06-27 12:36:41 +0000

    Part 1: Create frontend and backend services

---
+++ backend/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return {
+        "message": "Hello from Backend!",
+        "version": "1.0"
+    }
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001)
---
+++ frontend/app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+import requests
+
+app = Flask(__name__)
+
+BACKEND_URL = "http://localhost:5001"
+
+@app.route("/")
+def home():
+    response = requests.get(BACKEND_URL)
+    data = response.json()
+
+    return f"""
+    <h1>Simple DevOps Demo</h1>
+
+    <p><b>Backend Message:</b> {data["message"]}</p>
+    <p><b>Version:</b> {data["version"]}</p>
+    """
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
